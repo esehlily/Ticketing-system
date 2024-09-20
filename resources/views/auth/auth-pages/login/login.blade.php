92baf2378486fea87212da59f1aa2ac067734aaa
esehlily <esehlily@gmail.com>
2024-09-20 17:01:57 +0100

    third commit

--- resources/views/auth/auth-pages/login/login.blade.php
+++ resources/views/auth/auth-pages/login/login.blade.php
@@ -16,7 +16,7 @@
                 <form>
                     <div class="mb-3 ">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email address">
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div class="mb-2 position-relative">
